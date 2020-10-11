a014e88e21f7de36ee7792c35eb84337395dfa5f
AlfredoCU <alfredo_11_cu@hotmail.com>
2020-10-11 16:32:31 -0500

    Se creo la suma de los elementos que pertenecen a un arrglo.

--- main.go
+++ main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	// Variables.
+	var (
+		s []int
+		res int
+		value int
+		length int
+	)
+
+	// Length of slices
+	_, _ = fmt.Scan(&length)
+
+	// Insert data.
+	for i := 0; i < length; i++ {
+		_, _ = fmt.Scan(&value)
+		s = append(s, value)
+	}
+
+	// Sum.
+	for _, val := range s {
+		res += val
+	}
+
+	// Result
+	fmt.Println(res)
+
+	// Print array.
+	// fmt.Println(s)
+
+	// Print length and capacity.
+	// fmt.Println(len(s), cap(s))
 }
